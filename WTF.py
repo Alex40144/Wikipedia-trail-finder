d2ef2d7789a7e919b284b790a1ffdedaba1c4f7c
Alex40144 <giddyraccoon@gmail.com>
2020-07-20 17:32:49 +0100

    succesfully generates routes

--- WTF.py
+++ WTF.py
@@ -1,42 +1,72 @@
-import httplib2
+import urllib.request
 from bs4 import BeautifulSoup, SoupStrainer
 
-http = httplib2.Http()
+#TODO change to usesr input
+start = 'https://en.wikipedia.org/wiki/Nasa'
+target = 'https://en.wikipedia.org/wiki/NACA_airfoil'
+
+max_attempts = 4
 
 
-
-start = 'https://en.wikipedia.org/wiki/NASA'
-target = 'https://en.wikipedia.org/wiki/Curiosity_(rover)'
 StartPage = start[start.find('/wiki/'):]
 TargetPage = target[target.find('/wiki/'):]
-status, response = http.request(start)
-
-def GetLinksFromPage(page):
-    links = []
-    page = 'https://en.wikipedia.org' + page
-    status, response = http.request(page)
-    for link in BeautifulSoup(response, parse_only=SoupStrainer('mw-body-content'), features="html.parser"):
-        if link.has_attr('href'):
-            if link['href'][:6] == '/wiki/':
-                links.append(link['href'])
-    return links
 
 
+class WTF:
+    def __init__(self, StartPage, TargetPage, max_attempts):
+        self.StartPage = StartPage
+        self.TargetPage = TargetPage
+        self.max_attempts = max_attempts
+        self.paths = {StartPage:[StartPage]}
+        self.queue = [StartPage]
+        self.attempts = 0
 
-def loop(StartPage, TargetPage):
-    pages = []
-    print('Finding the shortest path between the %s and %s pages...' % (StartPage, TargetPage))
-    pages.append(GetLinksFromPage(StartPage))
-    print(pages)
-    print(TargetPage)
-    if TargetPage in pages:
-        print('page found after _ moves')
+    def GetLinksFromPage(self, page):
+        links = []
+        page = 'https://en.wikipedia.org' + page
+        try:
+            content = urllib.request.urlopen(page)
+        except urllib.error.HTTPError:
+            print("failed to load: " + page)
+            return None
+
+        soup = BeautifulSoup(content, "html.parser")
+        base=soup.find('div', id="bodyContent")
+
+        for link in BeautifulSoup(str(base), "html.parser").findAll("a"):
+            if 'href' in link.attrs:
+                if 'pedia' not in link['href'] and 'http' not in link['href'] and 'Category' not in link['href'] and '#' not in link['href'] and 'disambiguation' not in link['href'] and 'File' not in link['href']:
+                    links.append(link['href'])
+        return links
+
+
+
+    def loop(self):
+        print('Finding the shortest path between the {} and {} pages...'.format(StartPage, TargetPage))
+
+        while len(self.queue) and self.attempts < max_attempts:
+                currentPage = self.queue.pop(0)
+
+                currentPageLinks = self.GetLinksFromPage(currentPage)
+
+                #check if page is empty
+                if currentPageLinks == None:
+                    continue
+
+                #check if we have found Target
+                if self.TargetPage in currentPageLinks:
+                    print(self.paths[currentPage]+[self.TargetPage])
+                    self.attempts += 1
+
+                else:
+                    for link in currentPageLinks:
+                        if link not in self.paths[currentPage]:
+                            self.queue.append(link)
+                            self.paths[link] = self.paths[currentPage]+[link]
 
 
 
 
 
-
-
-
-loop(StartPage, TargetPage)
+game = WTF(str(StartPage),str(TargetPage), max_attempts)
+game.loop()
